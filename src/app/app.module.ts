310c53c63c62353fc1bad238d79ba14cd2655a30
Nataliia Odai <nataliiaodai@Nataliias-MacBook-Pro.local>
2022-12-16 16:07:41 +0200

    forms-upgraded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 import {DropdownDirective} from "./shared/dropdown.directive";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RecipeService} from "./recipes/recipe.service";
 
 
 
@@ -34,9 +37,12 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,10 +3,11 @@ import { Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
-
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe(
       'Christmas pudding',
@@ -14,14 +15,10 @@ export class RecipeService {
       'https://images.immediate.co.uk/production/volatile/sites/30/2017/11/stir-up-Christmas-pudding-c326564.jpg?quality=90&webp=true&resize=375,341',
    [
      new Ingredient('Raisins', 100),
-     new Ingredient('Currants', 100),
-     new Ingredient('Mixed candied citrus peel', 100),
-     new Ingredient('Glacé cherries, roughly chopped', 100),
      new Ingredient('Brandy', 50),
      new Ingredient('Flaked almonds', 50),
-     new Ingredient('Eggs, beaten', 2),
      new Ingredient('Gluten-free baking powder', 1),
-     new Ingredient('Dark brown soft sugar', 100),
+
    ] ),
     new Recipe(
       'Сhicken breasts',
@@ -29,11 +26,9 @@ export class RecipeService {
       'https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Airfryer-hero-image-2a442a6.jpg?quality=90&webp=true&resize=375,341',
       [
         new Ingredient ('Chicken breasts', 4),
-        new Ingredient ('Rapeseed oil', 1),
-        new Ingredient ('Garlic granules', 5),
         new Ingredient ('Smoked sweet paprika', 1),
         new Ingredient ('Mixed herbs', 2),
-        new Ingredient ('Pepper', 1),
+        new Ingredient ('Pepper', 10),
       ])
   ];
 
@@ -51,6 +46,21 @@ export class RecipeService {
     this.slService.addIngredients(ingredients);
 
   }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+    deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+    }
 
 }
 
